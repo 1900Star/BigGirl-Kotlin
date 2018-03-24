49d64b09ac8750560b81b15432d4acb0afc509d4
Luoshipeng <strangermy98@gmail.com>
2018-03-24 22:32:13 +0800

    列表滚动时隐藏Tabbar

---
+++ app/src/main/java/com/yibao/gankkotlin/base/listener/OnTabbarVisibleListener.kt
@@ -0,0 +1,14 @@
+package com.yibao.gankkotlin.base.listener
+
+
+/**
+ *  @项目名：  BigGirl-Kotlin
+ *  @包名：    com.yibao.gankkotlin.base.listener
+ *  @文件名:   OnTabbarVisibleListener
+ *  @author:   Stran
+ *  @Email:   www.strangermy@outlook.com / www.strangermy98@gmail.com
+ *  @创建时间:  2018/3/24 21:18
+ *  @描述：    {TODO}
+ */
+interface OnTabbarVisibleListener {
+}
